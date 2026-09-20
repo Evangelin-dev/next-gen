f4fefcee2eb2e9ed1c74c9a1bbe8f3d9493536c4
Tannu Nagar <tanu@gmail.com>
2026-09-20 08:04:52 +0530

    baseurl

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://app.thebotcrm.com/api",
   headers: {
     "Content-Type": "application/json",
   },
